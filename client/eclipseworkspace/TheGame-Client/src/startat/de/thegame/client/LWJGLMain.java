3f9eaee0a345cce249912421417d1edf1296f500
Arondc <sten.seifert@googlemail.com>
2014-09-12 19:58:49 +0200

    LWJGL Quad Test

--- client/eclipseworkspace/TheGame-Client/src/startat/de/thegame/client/LWJGLMain.java
+++ client/eclipseworkspace/TheGame-Client/src/startat/de/thegame/client/LWJGLMain.java
@@ -3,6 +3,7 @@ package startat.de.thegame.client;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class LWJGLMain {
 	public void start() {
@@ -13,8 +14,24 @@ public class LWJGLMain {
 			e.printStackTrace();
 			System.exit(0);
 		}
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	
 		while(!Display.isCloseRequested()){
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			GL11.glColor3f(0.5f,0.5f,1.0f);
+			 
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			    GL11.glVertex2f(100,100);
+			    GL11.glVertex2f(100+200,100);
+			    GL11.glVertex2f(100+200,100+200);
+			    GL11.glVertex2f(100,100+200);
+			GL11.glEnd();
+			
 			
 			Display.update();
 		}
